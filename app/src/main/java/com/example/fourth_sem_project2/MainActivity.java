2ffe788eb0eb064e54b387751027a51daa8edd92
Rahul Kumar <rahul@gmail.com>
2024-07-18 07:51:07 +0545

    Complete Menu

--- app/src/main/java/com/example/fourth_sem_project2/MainActivity.java
+++ app/src/main/java/com/example/fourth_sem_project2/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.fourth_sem_project2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -28,6 +32,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nav = findViewById(R.id.nav);
+
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id=menuItem.getItemId();
+                if (id==R.id.home){
+                    Intent i1=new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i1);
+                    Toast.makeText(MainActivity.this, "click to main item", Toast.LENGTH_SHORT).show();
+                }
+                if (id==R.id.about){
+                    Intent i2=new Intent(getApplicationContext(), About.class);
+                    startActivity(i2);
+                }
+                if (id==R.id.setting){
+                    Intent i3=new Intent(getApplicationContext(), Setting.class);
+                    startActivity(i3);
+                }
+                return false;
+            }
+        });
 
         final DrawerLayout drawerLayout = findViewById(R.id.main);
         findViewById(R.id.nav).setOnClickListener(new View.OnClickListener() {
